cc4bacad4ec6977f9d75fcbdfec27dcb280a9417
Estvexx <franciscoesteves2016@gmail.com>
2026-03-11 13:34:14 +0000

    fix: .env and bd connections

--- core/src/main/java/com/iogurtes/util/DatabaseConnection.java
+++ core/src/main/java/com/iogurtes/util/DatabaseConnection.java
@@ -1,15 +1,23 @@
 package com.iogurtes.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Properties;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DatabaseConnection {
 
-    private static final String URL      = "jdbc:postgresql://localhost:5432/iogurtes_db";
-    private static final String USER     = "postgres";
-    private static final String PASSWORD = "578999";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String user = dotenv.get("DB_USER");
+    private static final String password = dotenv.get("DB_PASSWORD");
+    private static final String ssl = dotenv.get("DB_SSL");
+    private static final String host = dotenv.get("DB_HOST");
+    private static final String port = dotenv.get("DB_PORT");
+    private static final String db_name = dotenv.get("DB_NAME");
+    private static final String URL = String.format(
+            "jdbc:postgresql://%s:%s/%s", host, port, db_name
+    );
 
     private static Connection connection;
 
@@ -27,9 +35,9 @@ public class DatabaseConnection {
 
         try {
             Properties props = new Properties();
-            props.setProperty("user", USER);
-            props.setProperty("password", PASSWORD);
-            props.setProperty("ssl", "false");
+            props.setProperty("user", user);
+            props.setProperty("password", password);
+            props.setProperty("ssl", ssl);
 
             connection = DriverManager.getConnection(URL, props);
             System.out.println("✅ Ligação à BD estabelecida!");
